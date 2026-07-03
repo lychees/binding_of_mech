fd3706f1eded3f6e224726389f5dac1f66341eba
minakokojima <lychees67@gmail.com>
2026-07-04 05:26:43 +0800

    修复 WEAPONS 导入路径

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // 主入口 - 游戏主循环和UI管理
 import { keys, bullets, particles, footprints, hooks, obstacles, enemies, setCamera, WORLD_WIDTH, WORLD_HEIGHT, CANVAS_WIDTH, CANVAS_HEIGHT } from './config.js';
-import { WEAPONS, ENEMY_TEMPLATES, LEVELS } from './enemies.js';
+import { ENEMY_TEMPLATES, LEVELS } from './enemies.js';
+import { WEAPONS } from './weapons.js';
 import { loadSave, saveGame, addMoneyExp, upgradeCost, getMechStats } from '../data/save.js';
 import Mech from './classes/Mech.js';
 import Enemy from './classes/Enemy.js';
